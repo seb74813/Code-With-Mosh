33258a7954d205c1ec866bdf9590f4cda7dd14de
seb74813 <39569227+seb74813@users.noreply.github.com>
2020-07-07 17:06:35 -0400

    Initial commit

---
+++ PhotoGallery/PhotoGallery/PhotoGallery/GalleryPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhotoGallery
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GalleryPage : ContentPage
+    {
+        public int id;
+        
+        public GalleryPage()
+        {
+            InitializeComponent();
+            id = 1;
+            photo.Source = "http://lorempixel.com/1920/1080/city/" + id; 
+        }
+
+        private void Left_Button_Clicked(object sender, EventArgs e)
+        {
+            id--;
+            if (id == 0)
+            { id = 10; }
+            photo.Source = "http://lorempixel.com/1920/1080/city/" + id;
+        }
+
+        private void Right_Button_Clicked(object sender, EventArgs e)
+        {
+            id++;
+            if (id == 11)
+            { id = 1;  }
+            photo.Source = "http://lorempixel.com/1920/1080/city/" + id;
+        }
+    }
+}
---
+++ Quotes/Quotes/Quotes/QuotesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Quotes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        String[] quotes;
+        int index;
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+
+            quotes = new string[]
+                {
+                    "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+                    "You can't blame gravity for falling in love.",
+                    "Look deep into nature, and then you will understand everything better."
+                };
+
+            label.Text = quotes[index];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            index++;
+            if (index == quotes.Length)
+            { index = 0; }
+            label.Text = quotes[index];
+        }
+    }
+}
