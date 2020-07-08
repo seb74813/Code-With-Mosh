aca4b36c680c5e3a523612c7530dad965bf9f4d9
seb74813 <39569227+seb74813@users.noreply.github.com>
2020-07-07 21:11:48 -0400

    ListView Excercise

---
+++ Airbnb/Airbnb/Airbnb/Models/SearchGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Airbnb.Models
+{
+    class SearchGroup : List<Search>
+    {
+        public string Title;
+        public SearchGroup(string name)
+        {
+            Title = name;
+        }
+    }
+}
---
+++ Airbnb/Airbnb/Airbnb/SearchPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using Airbnb.Models;
+
+namespace Airbnb
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        ObservableCollection<Search> searches;
+
+        public SearchPage()
+        {
+
+            InitializeComponent();
+
+            listView.ItemsSource = GetSearches();
+        }
+
+        private IEnumerable<Search> GetSearches(string filter = null)
+        {
+            searches = new ObservableCollection<Search>
+            {
+                new Search { Id = 0, Location = "West Hollywood, CA, United States", CheckIn = new DateTime(2020, 9, 1), CheckOut = new DateTime(2020, 11, 1)},
+                new Search { Id = 1, Location = "Santa Monica, CA, United States", CheckIn = new DateTime(2020, 9, 1), CheckOut = new DateTime(2020, 11, 1)}
+            };
+
+            if (string.IsNullOrEmpty(filter))
+            { return searches; }
+
+            return searches.Where<Search>(c => c.Location.StartsWith(filter));
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var search = e.SelectedItem as Search;
+            DisplayAlert("Selected", search.Location, "OK");
+        }
+
+        private void MenuItem_Clicked(object sender, EventArgs e)
+        {
+            var search = (sender as MenuItem).CommandParameter as Search;
+            searches.Remove(search);
+        }
+
+        private void listView_Refreshing(object sender, EventArgs e)
+        {
+            listView.ItemsSource = GetSearches();
+
+            listView.EndRefresh();
+        }
+
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listView.ItemsSource = GetSearches(e.NewTextValue);
+        }
+    }
+}
